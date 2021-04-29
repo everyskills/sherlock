a9a00716f51271273d1c9a5ff92149586142f332
Osama AL-zabidi <74067162+everyskills@users.noreply.github.com>
2021-04-30 01:32:38 +0300

    Update __main__.py

--- sherlock/__main__.py
+++ sherlock/__main__.py
@@ -9,7 +9,6 @@ networks.
 
 import sys
 
-
 if __name__ == "__main__":
     # Checking if the user is using the correct version of Python
     # Reference:
@@ -17,13 +16,13 @@ if __name__ == "__main__":
     #               major --^
     #               minor ----^
     #               micro ------^
-    major = sys.version_info[0]
-    minor = sys.version_info[1]
 
-    python_version = str(sys.version_info[0])+"."+str(sys.version_info[1])+"."+str(sys.version_info[2])
+    # python_version = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2])
+    python_version = '.'.join(sys.version_info[:3])
 
-    if major != 3 or major == 3 and minor < 6:
-        print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
+    # if major != 3 or major == 3 and minor < 6:
+    if (sys.version_info[0] <= 3 and sys.version_info[1] < 6):
+        print(" ! Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
         sys.exit(1)
 
     import sherlock
